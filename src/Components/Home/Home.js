e72221efa7f66a2a766f78e3463ed2d0a36a156a
Ritika Saxena <ritikasaxena911@gmail.com>
2023-06-15 16:02:52 +0530

    added home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,54 +5,62 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { useEffect } from 'react';
+import { useState } from 'react'
+import './Home.css'
+
 
 export default function ActionAreaCard() {
     const styles={textAlign:"start",fontWeight:"600",fontSize:"17px"};
     const s={textAlign:"start"}
+    const [result,setResult]= useState([]);
 
-    useEffect(()=>{
-    // const showvideo=()=>{
-      const params={
-        relatedToVideoId: '7ghhRHRP6t4',
-        part: 'id,snippet',
-        type: 'video',
-        maxResults: '50'
-      }
-      const queryString = new URLSearchParams(params).toString();
-      fetch(`https://youtube-v31.p.rapidapi.com/search?${queryString}`,{
+    const showvideo=()=>{
+      fetch("https://youtube-v3-alternative.p.rapidapi.com/trending",{
         method:"get",
         headers: {
           'X-RapidAPI-Key': '894026b4c9msh3adf9faf2418584p15519cjsn0996d882981d',
-          'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+          'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
         }
 
       }).then(res=>res.json())
       .then(data=>{
+        setResult(data.data)
         console.log(data)
       }).catch(err=>console.log(err))
-    // }
-  })
+}
+useEffect(()=>{
+  showvideo()
+},[])
   return (
+    <>
+    <section className='Vid'>
+     { 
+     result.map(videos=>( 
     <Card sx={{ maxWidth: 345, marginTop:"2rem", marginLeft:"3rem", height:"20rem" }}
     >
       <CardActionArea>
         <CardMedia
           component="img"
           height="200"
-          image="https://i.ytimg.com/vi/KKQxya-N6A0/maxresdefault.jpg"
+          image={videos.thumbnail[2].url}
         />
         <CardContent>
           <Typography sx={styles} gutterBottom variant="h6" component="div">
-          Srikant Vs The Kids | The Family Man | Srikant, Dhriti, Atharv | Prime Video
+          {/* Srikant Vs The Kids | The Family Man | Srikant, Dhriti, Atharv | Prime Video */}
+           {videos.title}
           </Typography>
           <Typography variant="body2" sx={s} color="text.secondary">
-            Prime Video India
+            {videos.channelTitle}
           </Typography>
           <Typography variant="body2" sx={s} color="text.secondary">
-            3.5M Views . 4 months ago
+            {videos.viewCount} Views . {videos.publishedText}
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
+      )) 
+     } 
+    </section>
+    </>
   );
 }
